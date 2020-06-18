5cd1821fbe8821e21b02405ca281ca9c7158bdc7
ThomasS <thomas.stessels@gmail.com>
2020-06-18 13:58:11 +0200

    models mongoose setup

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+    email: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    birthday: {
+      type: Date
+      //required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    }
+  });
+
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
